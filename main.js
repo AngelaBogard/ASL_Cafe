982e1989883900027785ed9953cebbc2cc36c19d
angelabogard7@gmail.com <angelabogard7@gmail.com>
2021-10-16 23:04:01 -0500

    update js

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ while (name == 'null') {
 if (name !== 'null') {
     alert("Welcome, " + name + "!");
 }
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
