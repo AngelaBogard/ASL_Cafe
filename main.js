751d5e22199f19bb81e9f4cc202a3ffd425caca1
angelabogard7@gmail.com <angelabogard7@gmail.com>
2021-10-17 13:55:55 -0500

    fixed some errors

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-var name = prompt("Please tell me your name: ");
-while (name == 'null') {
-    name = prompt("Please tell me your name: ");
-}
-if (name !== 'null') {
-    alert("Welcome, " + name + "!");
-}
-
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
