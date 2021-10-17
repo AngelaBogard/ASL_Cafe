742189e2c137c994582b491b029bbeb116144ba8
angelabogard7@gmail.com <angelabogard7@gmail.com>
2021-10-17 13:47:30 -0500

    made some changes to the codes

--- main.js
+++ main.js
@@ -10,3 +10,23 @@ var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
+
+var greeting = document.getElementById("greeting");
+var day = new Date();
+var hr = day.getHours();
+if (hr >= 0 && hr < 12) {
+  greeting.textContent = ("Good Morning!");
+} else if (hr == 12) {
+  greeting.textContent = ("Good Noon!");
+} else if (hr >= 12 && hr <= 17) {
+  greeting.textContent = ("Good Afternoon!");
+} else {
+  greeting.textContent = ("Good Evening!");
+}
+
+
+var myfrm = document.getElementById("myfrm");
+myfrm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("You submitted the form!");
+});
