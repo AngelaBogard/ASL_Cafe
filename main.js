b6d32df092f65a8c42dd604d86dd976676c98bb2
angelabogard7@gmail.com <angelabogard7@gmail.com>
2021-12-29 21:03:02 -0600

    made changes to loop

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
-
-var greeting = document.getElementById("greeting");
-var day = new Date();
-var hr = day.getHours();
-if (hr >= 0 && hr < 12) {
-  greeting.textContent = ("Good Morning!");
-} else if (hr == 12) {
-  greeting.textContent = ("Good Noon!");
-} else if (hr >= 12 && hr <= 17) {
-  greeting.textContent = ("Good Afternoon!");
-} else {
-  greeting.textContent = ("Good Evening!");
+function getGreeting() {
+  var day = new Date();
+  var hr = day.getHours();
+  if (hr >= 0 && hr < 12) { 
+    return "Good Morning!";
+  } else if (hr == 12) {
+    return "Good Noon!";
+  } else if (hr >= 12 && hr <= 17) {
+    return "Good Afternoon!";
+  } else {
+    return "Good Evening!";
+  }
 }
-
+var greeting = document.getElementById("greeting");
+greeting.textContent = getGreeting();
 
 var myfrm = document.getElementById("myfrm");
 myfrm.addEventListener("submit", function(event) {
@@ -23,9 +20,36 @@ myfrm.addEventListener("submit", function(event) {
   alert("You submitted the form!");
 });
 
-
-for (var counter = 1; counter < 5; counter++) {
-    console.log('I love Coffee!:' + counter);
 }
-console.log('Come get some!:' + counter);
+function myFunction() {
+    var input = document.getElementById("result").value;
+    if (input == 2) {
+        document.getElementById("answer").innerHTML = "Right!"
+    } else {
+        document.getElementById("answer").innerHTML = "Try again!"
+  }
+}
+var cafeForm = document.getElementById("cafe");
+cafeForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  myFunction();
+});
+
+function secondFunction() {
+    var drinklocal = document.getElementById("question").value;
+    var parent = document.getElementById('image');
+    var imagePath = "Coffeecup.jpeg";
+
+    for (let i = drinklocal; i > 0; i--) {
+        var img = new Image();
+        img.src = imagePath;
+        parent.appendChild(img);
+  }
+}
+
+var deafjavaForm = document.getElementById("deafjava");
+deafjavaForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  secondFunction();
+});
 
